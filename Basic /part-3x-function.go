f1e4ab6158f7910f554c89e64ca7567659326bbe
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-07-15 22:01:47 +0100

    Add more Go basic programming code

---
+++ Basic /part-3x-function.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// We can declare both parameter as a type
+
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func multiplyTwo(x, y int) int {
+	return x * y
+}
+
+func movieInformation(movie string) (int, bool, string) {
+	var productionCost int = 0
+	var englishLanguage bool = false
+	var director string = "Unknown"
+
+	if movie == "Harry Potter" {
+		productionCost, englishLanguage, director = 1870000, true, "James Bolt"
+		return productionCost, englishLanguage, director
+	}
+	return productionCost, englishLanguage, director
+}
+
+func location(city string) (region, continent string) {
+	switch city {
+	case "Los Angeles", "LA", "Santa Monica":
+		region, continent = "California", "North America"
+	case "New York", "NYC":
+		region, continent = "New York", "North America"
+	default:
+		region, continent = "Unknown", "Unknown"
+	}
+	return
+}
+
+func main() {
+	region, continent := location("Santa Monica")
+	fmt.Printf("Matt lives in %s, %s", region, continent)
+}
---
+++ resources/Tutorials.md
@@ -0,0 +1,3 @@
+# A list of useful Go tutorial 
+
+https://astaxie.gitbooks.io/build-web-application-with-golang/en/
